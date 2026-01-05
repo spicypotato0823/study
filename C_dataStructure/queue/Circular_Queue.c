0773ee303d543b9b86e3e4486f0a56456b7c46d7
spicypotato0823 <doyunkim1234@gmail.com>
2026-01-05 22:15:37 +0900

    window : queue

---
+++ C_dataStructure/queue/Circular_Queue.c
@@ -0,0 +1,117 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_QUEUE_SIZE 5
+
+typedef struct QueueType{
+    
+    int data[MAX_QUEUE_SIZE];
+    int rear;
+    int front;
+
+}QueueType;
+
+void init_queue(QueueType * Q)
+{
+    Q->rear = 0;
+    Q->front = 0;
+}
+
+int is_empty(QueueType * Q)
+{
+    if(Q->front == Q->rear)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int is_full(QueueType * Q)
+{
+    if(Q->front == ((Q->rear + 1) % MAX_QUEUE_SIZE))
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+    // 원형 큐에서 배열 한 칸을 비워두는 이유는 공백 상태와 포화 상태를 구별하기 위함. 
+}
+
+void enqueue(int data, QueueType * Q)
+{
+    if(is_full(Q))
+    {
+        printf("Queue is full\n");
+        exit(1);
+    }
+
+    Q->rear = (Q->rear + 1) % MAX_QUEUE_SIZE;
+    Q->data[Q->rear] = data; 
+    
+    return;
+}
+
+int dequeue(QueueType * Q)
+{
+    if(is_empty(Q))
+    {
+        printf("Queue is empty\n");
+        exit(1);
+    }
+
+    Q->front = (Q->front + 1) % MAX_QUEUE_SIZE;
+    return Q->data[Q->front];
+}
+
+void print_queue(QueueType * Q)
+{
+    printf("Q->front : %d , Q->rear : %d\n", Q->front, Q->rear);
+
+    for(int i = 0; i < MAX_QUEUE_SIZE; i++)
+    {
+        printf("[data %d] : %d\n", i, Q->data[i]);
+    }
+
+    return;
+}
+
+int main(void)
+{
+    QueueType queue1;
+
+    init_queue(&queue1);
+
+    enqueue(10, &queue1);
+    
+    print_queue(&queue1);
+
+    enqueue(20, &queue1);
+    
+    print_queue(&queue1);
+    
+    enqueue(30, &queue1);
+    
+    print_queue(&queue1);
+
+    enqueue(40, &queue1);
+
+    print_queue(&queue1);
+
+    dequeue(&queue1);
+    
+    print_queue(&queue1);
+
+    dequeue(&queue1);
+    
+    print_queue(&queue1);
+
+    enqueue(60, &queue1);
+
+    print_queue(&queue1);
+
+}
