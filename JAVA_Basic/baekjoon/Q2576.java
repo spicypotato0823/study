1faccc86625ded4f9ba135447a8581a7a3dacd92
spicypotato0823 <doyunkim1234@gmail.com>
2026-02-01 19:28:23 +0900

    win : baekjoon

---
+++ JAVA_Basic/baekjoon/Q2576.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+
+public class Q2576 
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int j = 0;
+        int enter = 0;
+        int sum = 0;
+        int min = 100;
+
+        for(int i = 0; i < 7; i++)
+        {
+            enter = (Integer.parseInt(bf.readLine()));
+            
+            if(enter % 2 != 0)
+            {
+                sum += enter;
+                if(min > enter)
+                {
+                    min = enter;
+                }
+
+                j++;
+            }
+        }
+        if(j == 0)
+        {
+            System.out.println(-1);
+        }
+        else
+        {
+            System.out.println(sum);
+            System.out.println(min);
+        }
+
+    }    
+}
---
+++ JAVA_Basic/baekjoon/Q2745.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class Q2745 
+{
+    public static void main(String[] args) throws IOException
+    {
+        System.out.println('A' - '0');
+    }    
+}
