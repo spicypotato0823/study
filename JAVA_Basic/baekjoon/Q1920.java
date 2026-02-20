6d6ccb4f606507e61ca86cfe1dd4a7c976c0f397
spicypotato0823 <doyunkim1234@gmail.com>
2026-02-20 22:08:33 +0900

    mac: baekjoon

--- JAVA_Basic/baekjoon/Q1920.java
+++ JAVA_Basic/baekjoon/Q1920.java
@@ -1,40 +1,55 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Q1920
-{
-    public static void main(String[] args) throws IOException
+public class Q1920 {
+    public static void main(String[] args) throws IOException 
     {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        
         int n = Integer.parseInt(bf.readLine());
-        int [] arr = new int[n];
-        int p;
+        int[] arr = new int[n];
         StringTokenizer st = new StringTokenizer(bf.readLine());
 
-        for(int i = 0; i < n; i++)
-        {
-            int target = Integer.parseInt(st2.nextToken()); // 타겟
+        for (int i = 0; i < n; i++) 
+            {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-
+        
+        Arrays.sort(arr);
         int m = Integer.parseInt(bf.readLine());
         StringTokenizer st2 = new StringTokenizer(bf.readLine());
+        
+        StringBuilder sb = new StringBuilder();
 
-        for(int i = 0; i < m; i++)
-        {
-            p = 0;
-            for(int j = 0; j < n; j++)
+        for (int i = 0; i < m; i++) 
             {
-                if(Integer.parseInt(st2.nextToken()) == arr[j])
-                {
-                    p = 1;
+            int target = Integer.parseInt(st2.nextToken());
+            int left = 0;
+            int right = n - 1;
+            int p = 0;
+            int t;
+
+            while (left <= right) { 
+                t = (left + right) / 2;
+
+                if (arr[t] > target) {
+                    right = t - 1;
+                } 
+                else if (arr[t] < target) {
+                    left = t + 1;
+                } 
+                else {
+                    p = 1; 
                     break;
                 }
             }
-            System.out.println(p);
+            
+            sb.append(p).append("\n");
         }
+        
+        System.out.println(sb);
     }    
 }
