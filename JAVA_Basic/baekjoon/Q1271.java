3327964118deb9bc3d41c95cbaef7dc09379210b
spicypotato0823 <doyunkim1234@gmail.com>
2026-03-12 13:04:05 +0900

    mac:baekjoon

---
+++ JAVA_Basic/baekjoon/Q1271.java
@@ -0,0 +1,23 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Q1271
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
+        
+        BigInteger n = new BigInteger(st.nextToken());
+        BigInteger m = new BigInteger(st.nextToken());
+
+        System.out.println(n.divide(m));
+        System.out.println(n.remainder(m));
+        
+
+    }
+}
---
+++ JAVA_Basic/baekjoon/Q1874.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class Q1874 
+{
+    public static void main(String[] args) throws IOException
+    {
+        
+    }    
+}
