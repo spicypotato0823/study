9e9f45418783ab789cfc51af74a2ae1b59765f58
spicypotato0823 <doyunkim1234@gmail.com>
2026-03-16 10:47:59 +0900

    mac:baekjoon

--- JAVA_Basic/baekjoon/Q1874.java
+++ JAVA_Basic/baekjoon/Q1874.java
@@ -1,9 +1,45 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Q1874 
 {
     public static void main(String[] args) throws IOException
     {
+
+        StringBuilder sb = new StringBuilder();
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Integer> stack = new Stack<>();
+
+        int start = 1; // stack에 들어갈 수
         
-    }    
-}
+        int n = Integer.parseInt(bf.readLine());
+
+        for(int i = 0; i < n; i++)
+        {
+            int input = Integer.parseInt(bf.readLine());
+
+            while(start <= input)
+            {
+                stack.push(start);
+                start++;
+                sb.append("+\n");
+            }
+
+            if(stack.peek() == input)
+            {
+                sb.append("-\n");
+                stack.pop();
+            }
+            else
+            {
+                sb = new StringBuilder();
+                sb.append("NO");
+                break;
+            }
+        }
+       
+        System.out.println(sb);
+    }
+}    
