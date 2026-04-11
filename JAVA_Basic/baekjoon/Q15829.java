949cf61b60bc940cf8dbfd3e4919bc046e51620a
doyun <doyunkim1234@naver.com>
2026-04-11 17:30:24 +0900

    Q15829

--- JAVA_Basic/baekjoon/Q15829.java
+++ JAVA_Basic/baekjoon/Q15829.java
@@ -9,24 +9,19 @@ public class Q15829
     {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int l = Integer.parseInt(bf.readLine());
-        int r = 31;
-        BigInteger m = new BigInteger("1234567891");
-        BigInteger H = new BigInteger("0");
+        long r = 1;
+        long H = 0;
+        long M = 1234567891;
+
         String input = bf.readLine();
 
         for(int i = 0; i < l; i++)
         {
             int a = input.charAt(i) - 96;
-            BigInteger ar = new BigInteger("1");
-            ar = ar.multiply(BigInteger.valueOf(a));
-            for(int j = 0; j < i; j++)
-            {
-                ar = ar.multiply(BigInteger.valueOf(r));
-            }
-            H = H.add(ar);
+            long term = (a * r) % M;
+            H =(H + term)% M;
+            r = ( r * 31 )% M;
         }
-        H = H.mod(m);
-
         System.out.println(H);
     }
 
